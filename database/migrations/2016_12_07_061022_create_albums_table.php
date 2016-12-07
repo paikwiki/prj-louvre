1f3c1f8fab91459c2755e2e3ddaf1aec6805dcdc
work2do <wheretogowhentostop@gmail.com>
2016-12-07 15:16:00 +0900

    albums migration

---
+++ database/migrations/2016_12_07_061022_create_albums_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAlbumsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('albums', function (Blueprint $table) {
+            $table->increments('abm_seq')->unique();
+            $table->integer('usr_seq');
+            $table->integer('atw_seq');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('albums');
+    }
+}
