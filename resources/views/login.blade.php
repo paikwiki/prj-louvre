aedb614448c4a3d2beb2ea214c38934fb5f6b904
paikwiki <paikwiki@gmail.com>
2016-12-09 00:29:09 +0900

    add css,less to public/css/

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -4,10 +4,10 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" type="text/css" href="css/reset.css">
-  <link rel='stylesheet/less' type='text/css' href='css/styless.less'>
-	<script src="css/less.min.js" type="text/javascript"></script>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" type="text/css" href="/css/reset.css">
+  <link rel='stylesheet/less' type='text/css' href='/css/styless.less'>
+	<script src="/css/less.min.js" type="text/javascript"></script>
+  <link rel="stylesheet" type="text/css" href="/css/style.css">
   <title>Login</title>
 </head>
 <body>
