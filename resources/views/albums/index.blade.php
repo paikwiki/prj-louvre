82db9459d727e63eee4d57708c875c97789e6504
paikwiki <paikwiki@gmail.com>
2016-12-09 11:33:31 +0900

    frontend 수정

--- resources/views/albums/index.blade.php
+++ resources/views/albums/index.blade.php
@@ -1,56 +1,41 @@
-<!DOCTYPE html>
-<html lang="ko">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <link rel="stylesheet" type="text/css" href="css/reset.css">
-  <link rel='stylesheet/less' type='text/css' href='css/styless.less'>
-	<script src="css/less.min.js" type="text/javascript"></script>
-  <link rel="stylesheet" type="text/css" href="css/style.css">
-  <title>Albums</title>
-</head>
-<body>
-<div class="g-wrapper">
-  <div class="header">
-    <div class="logo-box">
-      <h1 class="logo"><a href="2-1.html">Louvre</a></h1>
-    </div>
-    <ul>
-      <li><a href="2-1.html">학생리스트</a></li>
-      <li><a href="3-1.html">검색</a></li>
-      <li><a href="2-6.html">촬영</a></li>
-      <li><a href="5-1.html">앨범</a></li>
-      <li><a href="6-1.html">설정</a></li>
-    </ul>
-  </div>
-  <div class="c-wrapper">
-    <div class="content">
-      <div class="album-list-box">
-        <ul>
-          <li>
-            <a href="5-2.html">
-              <figure>
-                <img src="http://placehold.it/30x30" alt="">
-              </figure>
-              <p>전체사진보기</p>
-            </a>
-          </li>
-          <li>
-            <a href="5-2.html">
-              <figure>
-                <img src="http://placehold.it/30x30" alt="">
-              </figure>
-              <p>즐겨찾기</p>
-            </a>
-          </li>
-        </ul>
-      </div>
-    </div> <!-- /.content -->
-  </div> <!-- /.c-wrapper -->
-  <div class="footer">
-    <!-- no data -->
-  </div> <!-- /.footer -->
-</div> <!-- /.g-wrapper -->
-</body>
-</html>
+@extends('layouts.master')
+
+@section('header')
+<div class="logo-box">
+  <h1 class="logo"><a href="2-1.html">Louvre</a></h1>
+</div>
+<ul>
+  <li><a href="2-1.html">학생리스트</a></li>
+  <li><a href="3-1.html">검색</a></li>
+  <li><a href="2-6.html">촬영</a></li>
+  <li><a href="5-1.html">앨범</a></li>
+  <li><a href="6-1.html">설정</a></li>
+</ul>
+@endsection
+
+@section('content')
+<div class="album-list-box">
+  <ul>
+    <li>
+      <a href="5-2.html">
+        <figure>
+          <img src="http://placehold.it/30x30" alt="">
+        </figure>
+        <p>전체사진보기</p>
+      </a>
+    </li>
+    <li>
+      <a href="5-2.html">
+        <figure>
+          <img src="http://placehold.it/30x30" alt="">
+        </figure>
+        <p>즐겨찾기</p>
+      </a>
+    </li>
+  </ul>
+</div>
+@endsection
+
+@section('footer')
+
+@endsection
