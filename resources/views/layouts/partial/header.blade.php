e01da9125cb8d56b6a21bfae3a061cb4fca1c1d4
urustin <urustin@naver.com>
2016-12-12 18:17:24 +0900

    header edit

--- resources/views/layouts/partial/header.blade.php
+++ resources/views/layouts/partial/header.blade.php
@@ -5,11 +5,11 @@
     </div>
     @if(auth()->check())
       <ul class="clearfix">
-        <li class="item01" data-item="item-1"><a href="/students">학생리스트</a></li>
-        <li class="item02" data-item="item-2"><a href="/artworks">검색</a></li>
-        <li class="item03" data-item="item-3"><a href="/artworks/create">촬영</a></li>
-        <li class="item04" data-item="item-4"><a href="/albums">앨범</a></li>
-        <li class="item05" data-item="item-5"><a href="/settings">설정</a></li>
+        <li class="item01"><a href="/students">학생리스트</a></li>
+        <li class="item02"><a href="/artworks">검색</a></li>
+        <li class="item03"><a href="/artworks/create">촬영</a></li>
+        <li class="item04"><a href="/albums">앨범</a></li>
+        <li class="item05"><a href="/settings">설정</a></li>
       </ul>
     @endif
   </div>
