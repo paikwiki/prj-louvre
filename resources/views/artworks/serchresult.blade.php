f7a177428a102c564bc90de7217a76ccca5a485c
paikwiki <paikwiki@gmail.com>
2016-12-15 02:28:27 +0900

    merge 완료

--- resources/views/artworks/serchresult.blade.php
+++ resources/views/artworks/serchresult.blade.php
@@ -15,7 +15,7 @@
   <div class="search-result-artworks">
     @foreach( $result_aw_names as $result_name )
       <figure class="a-item">
-        <a href="/artworks/{{ $result_name->id }}"><img src="http://{{ $result_name->photo ? $result_name->photo : 'placehold.it/120x120' }}" alt=""></a>
+        <a href="/artworks/{{ $result_name->id }}"><img src="{{ $result_name->photo }}" alt=""></a>
         <figcaption><a href="/artworks/{{ $result_name->id }}">{{ $result_name->name }}</a></figcaption>
       </figure>
     @endforeach
@@ -26,6 +26,7 @@
 @if($op_val ==1)
 <div class="search-result-box">
   <div class="search-result-text">
+    <p>총 {{$sum_result or 'Default'}}개의 결과가 있습니다.</p>
     <p>날짜검색결과:
       @foreach($result_a_d as $result_date)
       {{$result_date->name or 'Default'}} &nbsp;&nbsp;
@@ -34,7 +35,7 @@
     <div class="search-result-artworks">
       @foreach( $result_a_d as $result_date )
         <figure class="a-item">
-          <a href="/artworks/{{ $result_date->id }}"><img src="http://{{ $result_date->photo ? $result_date->photo : 'placehold.it/120x120' }}" alt=""></a>
+          <a href="/artworks/{{ $result_date->id }}"><img src="{{ $result_date->photo }}" alt=""></a>
           <figcaption><a href="/artworks/{{ $result_date->id }}">{{ $result_date->name }}</a></figcaption>
         </figure>
       @endforeach
@@ -44,6 +45,7 @@
 @if($op_val==2)
 <div class="search-result-box">
   <div class="search-result-text">
+    <p>총 {{$sum_result or 'Default'}}개의 결과가 있습니다.</p>
     <p>유형검색결과:
       @foreach($result_types as $result_type)
       {{$result_type->name or 'Default'}}&nbsp;&nbsp;
@@ -52,7 +54,7 @@
   <div class="search-result-artworks">
     @foreach( $result_types as $result_type )
       <figure class="a-item">
-        <a href="/artworks/{{ $result_type->id }}"><img src="http://{{ $result_type->photo ? $result_type->photo : 'placehold.it/120x120' }}" alt=""></a>
+        <a href="/artworks/{{ $result_type->id }}"><img src="{{ $result_type->photo }}" alt=""></a>
         <figcaption><a href="/artworks/{{ $result_type->id }}">{{ $result_type->name }}</a></figcaption>
       </figure>
     @endforeach
@@ -63,6 +65,7 @@
 @if($op_val==3)
 <div class="search-result-box">
   <div class="search-result-text">
+    <p>총 {{$sum_result or 'Default'}}개의 결과가 있습니다.</p>
     <p>태그검색결과:
       @foreach($result_tags as $result_tag)
       {{$result_tag->name or 'Default'}} &nbsp;&nbsp;
@@ -71,7 +74,7 @@
   <div class="search-result-artworks">
     @foreach( $result_tags as $result_tag )
       <figure class="a-item">
-        <a href="/artworks/{{ $result_tag->id }}"><img src="http://{{ $result_tag->photo ? $result_tag->photo : 'placehold.it/120x120' }}" alt=""></a>
+        <a href="/artworks/{{ $result_tag->id }}"><img src="{{ $result_tag->photo }}" alt=""></a>
         <figcaption><a href="/artworks/{{ $result_tag->id }}">{{ $result_tag->name }}</a></figcaption>
       </figure>
     @endforeach
