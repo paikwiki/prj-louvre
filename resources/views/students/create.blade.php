a6c58be0537c8bb2cc31d69da4357ed365145c85
SoojiNoh <thensj@hanmail.net>
2017-01-19 16:09:14 +0900

    image_preview in student create

--- resources/views/students/create.blade.php
+++ resources/views/students/create.blade.php
@@ -17,11 +17,18 @@
       <div class="s-box1">
         <div class="top-left">
           <ul>
-             <li class="addpicture">
+            @if( empty($student->profile_pic) ||  $student->profile_pic == 'default' )
+              <li class="addpicture">
                 <label for="imgInp"><img src='/image/final-image/icon_input_profile.png'></label>
-                <input type="file" id="imgInp" name="profile_pic" value="{{ old('profile_pic') }}">
+                <input type="file" id="imgInp" name="profile_pic" value="{{ old('profile_pic', $student->profile_pic) }}">
               </li>
-                <img id="blah" src="" alt="your image"/>
+              <img id="image_preview" src="" alt="your image"/>
+            @else
+              <li class="addpicture hide">
+                <label for="imgInp"><img id="image_preview" src="https://louvrebucket.s3.amazonaws.com/studentuploads/{{ $student->profile_pic }}" alt="your image" style="display: block;"/></label>
+                <input type="file" id="imgInp" name="profile_pic" value="{{ $student->profile_pic }}">
+              </li>
+            @endif
           </ul>
         </div>
         <div class="top-right">
